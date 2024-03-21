ca044151d1ed31c685bc482529574587eceb8ed6
Charu Sharma <charusharma1991@gmail.com>
2024-03-21 21:05:39 +0530

    Update others.tsx

--- src/pages/others.tsx
+++ src/pages/others.tsx
@@ -15,7 +15,7 @@ function Contacts(){
                     <ul>
                         <li>Faculty Research Grant from Fujitsu Research, 2023.</li>
 
-                        <li>Start-Up Grant (for faculty) from SERB, 2022.</li>
+                        <li>Start-Up Grant (for early career faculty) from SERB, 2022.</li>
 
                         <li>Financial Assistance Award from NeurIPS, 2020.</li>
 
@@ -34,15 +34,18 @@ function Contacts(){
                     <p>&nbsp;</p>
                     <p>Invited Talks</p>
                     <ul>
-                        <li>Talk on "Geometric Deep Learning for Graphs and Point Clouds" at IIIT Vadodara (4th Nov, 2022)</li>
-                        <li>Talks on "Graph Representation Learning" and "Representation Learning for 3D Point Clouds" at the Dept. of CSE, SVNIT, Surat (1-2nd Nov, 2022)</li>
-                        <li>Talk on "Significance of Graph Representation Learning" at the Department of Computer Science, Delhi University  (28th September, 2022)</li>
+                        <li>Expert Talk on "Synergizing Contrastive Learning and Optimal Transport for 3D Point Cloud Domain Adaptation" at Winter School of AI & TCS, IIT Jodhpur (11-12th Dec, 2023).</li>
+                        <li>Talk on "Geometric Deep Learning for Graphs and Point Clouds" at SCIS, University of Hyderabad (9th Oct, 2023).</li>
+                        <li>Experience talk at CODS-COMAD 2023 D & I track, IIT Bombay (6th January, 2023).</li>
+                        <li>Talk on "Geometric Deep Learning for Graphs and Point Clouds" at IIIT Vadodara (4th Nov, 2022).</li>
+                        <li>Talks on "Graph Representation Learning" and "Representation Learning for 3D Point Clouds" at the Dept. of CSE, SVNIT, Surat (1-2nd Nov, 2022).</li>
+                        <li>Talk on "Significance of Graph Representation Learning" at the Department of Computer Science, Delhi University  (28th September, 2022).</li>
                         <li>Talk on "Knowledge Graph Representation Learning" at Sahaj Software (28th July, 2022).</li>
                         <li>Talk on "Introduction to Machine Learning for Graphs" at I-Srishti, I-Hub, IIIT Hyderabad (3rd June, 2022).</li>
                         <li>Talk on "Geometric Deep Learning for graphs and point clouds" at NISER Bhubaneswar (11th April, 2022).</li>
                         <li>Talk on "Point Cloud Representation Learning" at 3D Vision Workshop, CVIT, IIITH (June, 2021).</li>
                         <li>Talks at TCS Research, IIIT Hyderabad, Mahindra University (Apr, 2021).</li>
-                        <li>Talk on "Self-Supervised Few-Shot Learning on Point clouds" in IITH ACM Machine Intelligence Group (Jan, 2021) and Computer Vision Talks (Feb, 2021).</li>
+                        <li>Talk on "Self-Supervised Few-Shot Learning on Point Clouds" in IITH ACM Machine Intelligence Group (Jan, 2021) and Computer Vision Talks (Feb, 2021).</li>
                         <li>Talks on Machine Learning and Deep Learning, Hebei Normal University, Shijiazhuang, China (June-July 2019).</li>
                         <li>Talk on "Introduction to Machine Learning" at one week workshop by J-Hub, JNTU Hyderabad on May 30, 2018.</li>
                     </ul>
