34686b53a8b1726ed95c4c5da0b7bddc91216c7f
Charu Sharma <charusharma1991@gmail.com>
2024-05-06 13:01:28 +0530

    Update students.tsx

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,1 +1,33 @@
+function Students(){
+    return (
+        <>
+        <div className="pageHeader">
+            <div className="headerContent">
+                <div className="sectionContainer">
+                    <h2 className="pageTitle">Students</h2>
+                </div>
+            </div>
+        </div>
+        <div className="pageContents">
+            <div className="section color-1">
+                <div className="sectionContainer studentContent">
+                    <div>
+                        <p><span style={{fontWeight:'700', fontSize:'18px'}}>Current Students</span></p>
+                        <ul>
+                            <li><span style={{fontWeight:'700'}}>MS by Research</span> Vivek Vardhan, Jayadratha Gayen, Himanshu Pal, Janaksinh</li>
+                            <li><span style={{fontWeight:'700'}}>Dual-degree</span> Kunal Bhosikar, Sriteja Reddy</li>
+                            <li><span style={{fontWeight:'700'}}>Honors:</span> Bachina Venkat Sai Pranav</li>
+                        </ul>
+                        <p><span style={{fontWeight:'700', fontSize:'18px'}}>Alumni</span></p>
+                        <ul>
+                            <li><span style={{fontWeight:'700'}}>MS by Research</span> Prayushi Mathur (co-advisor), Siddharth Katageri</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
 
+export default Students;
