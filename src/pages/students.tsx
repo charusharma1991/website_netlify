675d429fdd1b1430f30e39b1a20637117f372d8d
Charu Sharma <charusharma1991@gmail.com>
2024-09-29 20:12:17 +0530

    Update students.tsx

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -14,13 +14,13 @@ function Students(){
                     <div>
                         <p><span style={{fontWeight:'700', fontSize:'18px'}}>Current Students</span></p>
                         <ul>
-                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Vivek Vardhan, Jayadratha Gayen, Himanshu Pal, Ven Janaksinh Vanabhai</li>
-                            <li><span style={{fontWeight:'700'}}>Dual-Degree:</span> Kunal Bhosikar, Sriteja Reddy</li>
-                            <li><span style={{fontWeight:'700'}}>Honors:</span> Bachina Venkat Sai Pranav</li>
+                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Vivek Vardhan, Jayadratha Gayen, Himanshu Pal, Ven Janaksinh Vanabhai, Sanjay SJ, Akash J, Sreehari Rajan</li>
+                            <li><span style={{fontWeight:'700'}}>Dual-Degree:</span> Kunal Bhosikar, Vinit Mehta, Monish Singhal</li>
+                            <li><span style={{fontWeight:'700'}}>Honors:</span> Bachina Venkat Sai Pranav, Kushang Agarwal</li>
                         </ul>
                         <p><span style={{fontWeight:'700', fontSize:'18px'}}>Alumni</span></p>
                         <ul>
-                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Prayushi Mathur (co-advisor), Siddharth Katageri</li>
+                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Prayushi Mathur (co-advised), Siddharth Katageri</li>
                         </ul>
                     </div>
                 </div>
