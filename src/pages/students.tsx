ade417cf5bd407258e111ffffad6342658e33ab1
Charu Sharma <charusharma1991@gmail.com>
2026-05-20 13:14:47 +0530

    Update students.tsx

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -16,13 +16,13 @@ function Students(){
                         <ul>
                             <li><span style={{fontWeight:'700'}}>PhD:</span> Vivek Vardhan, Shefali Bajaj</li>
                             <li><span style={{fontWeight:'700'}}>MS by Research:</span> Sanjay SJ, Akash J, Sreehari Rajan</li>
-                            <li><span style={{fontWeight:'700'}}>Dual-Degree:</span> Kunal Bhosikar, Vinit Mehta, Monish Singhal, Prajas Wadekar</li>
-                            <li><span style={{fontWeight:'700'}}>Honors:</span> Kushang Agarwal</li>
+                            <li><span style={{fontWeight:'700'}}>Dual-Degree:</span> Vinit Mehta, Monish Singhal, Prajas Wadekar, Devshree Vyas, Samarth Rao</li>
+                            <li><span style={{fontWeight:'700'}}>Honors:</span> Aditya Gaur, Vikesh Kansal</li>
                         </ul>
                         <p><span style={{fontWeight:'700', fontSize:'18px'}}>Alumni</span></p>
                         <ul>
-                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Prayushi Mathur (co-advised), Nikhil Chandak (co-advised), Siddharth Katageri, Vivek Vardhan, Jayadratha Gayen, Himanshu Pal, Ven Janaksinh Vanabhai</li>
-                            <li><span style={{fontWeight:'700'}}>B.Tech Honors:</span> Bachina Venkat Sai Pranav </li>
+                            <li><span style={{fontWeight:'700'}}>MS by Research:</span> Prayushi Mathur (co-advised), Nikhil Chandak (co-advised), Siddharth Katageri, Vivek Vardhan, Jayadratha Gayen, Himanshu Pal, Ven Janaksinh Vanabhai, Kunal Bhosikar</li>
+                            <li><span style={{fontWeight:'700'}}>B.Tech Honors:</span> Bachina Venkat Sai Pranav, Kushang Agarwal </li>
                         </ul>
                     </div>
                 </div>
